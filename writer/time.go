613b7aac22847e30b14a1a45eab00e4e4bc3f404
Soreing <Soreing.dev@gmail.com>
2023-03-29 23:30:03 +0400

    implement time encoding functions

--- writer/time.go
+++ writer/time.go
@@ -1,15 +1,50 @@
 package writer
 
-import "time"
+import (
+	"time"
+)
 
-func WriteTime(dst []byte, s time.Time) (ln int) {
-	return
+func WriteTime(dst []byte, t time.Time) (ln int) {
+	ln = copy(dst[1:], t.Format(time.RFC3339Nano)) + 1
+	dst[0], dst[ln] = '"', '"'
+	return ln + 1
 }
 
-func WriteTimePtr(dst []byte, s *time.Time) (ln int) {
-	return
+func WriteTimePtr(dst []byte, t *time.Time) (ln int) {
+	if t != nil {
+		ln = copy(dst[1:], t.Format(time.RFC3339Nano)) + 1
+		dst[0], dst[ln] = '"', '"'
+		return ln + 1
+	} else {
+		dst[0] = 'n'
+		dst[1] = 'u'
+		dst[2] = 'l'
+		dst[3] = 'l'
+		return 4
+	}
 }
 
-func WriteTimes(dst []byte, ss []time.Time) (ln int) {
-	return
+func WriteTimes(dst []byte, ts []time.Time) (ln int) {
+	if ts != nil {
+		dst[0] = '['
+		ln++
+
+		if len(ts) > 0 {
+			ln += WriteTime(dst[ln:], ts[0])
+			for _, t := range ts[1:] {
+				dst[ln] = ','
+				ln++
+				ln += WriteTime(dst[ln:], t)
+			}
+		}
+
+		dst[ln] = ']'
+		return ln + 1
+	} else {
+		dst[0] = 'n'
+		dst[1] = 'u'
+		dst[2] = 'l'
+		dst[3] = 'l'
+		return 4
+	}
 }
